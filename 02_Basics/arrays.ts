450bc424aae4aea52dbb6ca8e17ea59a5ac2304c
muzaffarbhat07 <bhatmuzaffar32@gmail.com>
2024-06-14 16:20:02 +0530

    objects and arrays

--- 02_Basics/arrays.ts
+++ 02_Basics/arrays.ts
@@ -1,3 +1,23 @@
 const superHeros: string[] = [];
 superHeros.push("spider")
-superHeros.push("spider")
+superHeros.push(1)
+
+// const heroPower: number[] = [];
+const heroPower: Array<number> = [];
+heroPower.push("spider")
+heroPower.push(1)
+
+type User = {
+  name: string
+  age: number
+}
+
+// const allUsers: User[] = [];
+const allUsers: Array<User> = [];
+allUsers.push({name: "muz", age: 22});
+
+const colors: number[][] = [
+  [205, 163, 195]
+]
+
+//readonly array
--- 02_Basics/objects.ts
+++ 02_Basics/objects.ts
@@ -2,5 +2,55 @@ function createUser({name: string, isPaid: boolean}){}
 
 createUser({name:"muzaffar", isPaid: true})
 
+//  WEIRD BEHAVIOUR
+createUser({name:"muzaffar", isPaid: true, email: "muz@g.com"});
+let newUser = {name:"muzaffar", isPaid: true, email: "muz@g.com"};
+createUser(newUser);
 
+// TYPE ALIASES
+type User = {
+  name: string;
+  email: string;
+  isPaid: boolean;
+}
+function loginUser(user: User): User {
+  return user
+}
+const user = loginUser({name: "", email: "", isPaid: true})
 
+//READONLY, OPTIONAL
+type PaidUser = {
+  readonly _id: string    // readonly
+  name: string
+  email: string
+  isPaid: boolean
+  cardNumber?: string     // optional
+}
+
+const myUser: PaidUser = {
+  _id: "1234",
+  name: 'Muzaffar',
+  email: "m@m.in",
+  isPaid: true
+}
+
+myUser.email = "m@m.com";
+myUser._id = "345";
+
+//COMBINING TYPES
+type cardNumber = {
+  cardnumber: string
+}
+type cardDate = {
+  date: string
+}
+type cardDetails = cardNumber & cardDate & {
+  cvv: number
+}
+const carddetails: cardDetails = {
+  cardnumber: "1321546",
+  date: "12/26",
+  cvv: 235
+}
+
+export {}
