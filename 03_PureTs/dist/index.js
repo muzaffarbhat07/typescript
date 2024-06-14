fe5bf74ac34bf4db7e7c1c564c78b9eb98abc549
muzaffarbhat07 <bhatmuzaffar32@gmail.com>
2024-06-15 00:08:28 +0530

    class basics

--- 03_PureTs/dist/index.js
+++ 03_PureTs/dist/index.js
@@ -1,3 +1,13 @@
 "use strict";
 console.log("typescript is here");
 console.log("typescript is amazing");
+//CLASSES
+class User {
+    constructor(email, name) {
+        this.city = "";
+        this.email = email;
+        this.name = name;
+    }
+}
+const muzaffar = new User("m@m.com", "muzaffar");
+muzaffar.city = "kashmir";
--- 03_PureTs/src/index.ts
+++ 03_PureTs/src/index.ts
@@ -1,2 +1,17 @@
 console.log("typescript is here")
 console.log("typescript is amazing")
+
+
+//CLASSES
+class User {
+  email: string
+  name: string
+  city: string = ""
+  constructor(email: string, name: string) {
+    this.email = email;
+    this.name = name;
+  }
+}
+
+const muzaffar = new User("m@m.com", "muzaffar")
+muzaffar.city = "kashmir"
