8b8270d69f84cec864aa6ea09887e26848d6ddb4
muzaffarbhat07 <bhatmuzaffar32@gmail.com>
2024-06-15 00:15:07 +0530

    private and public in class

--- 03_PureTs/dist/index.js
+++ 03_PureTs/dist/index.js
@@ -4,10 +4,21 @@ console.log("typescript is amazing");
 //CLASSES
 class User {
     constructor(email, name) {
-        this.city = "";
+        this.city = "kashmir";
         this.email = email;
         this.name = name;
     }
 }
 const muzaffar = new User("m@m.com", "muzaffar");
-muzaffar.city = "kashmir";
+// muzaffar.city = "kashmir" //np
+// muzaffar.name
+//different syntax
+class User2 {
+    constructor(email, name, userId) {
+        this.email = email;
+        this.name = name;
+        this.userId = userId;
+        this.city = "kashmir";
+    }
+}
+const muzaffar2 = new User2("m@m.com", "muzaffar", 5465123);
--- 03_PureTs/src/index.ts
+++ 03_PureTs/src/index.ts
@@ -5,8 +5,8 @@ console.log("typescript is amazing")
 //CLASSES
 class User {
   email: string
-  name: string
-  city: string = ""
+  private name: string
+  readonly city: string = "kashmir"
   constructor(email: string, name: string) {
     this.email = email;
     this.name = name;
@@ -14,4 +14,18 @@ class User {
 }
 
 const muzaffar = new User("m@m.com", "muzaffar")
-muzaffar.city = "kashmir"
+// muzaffar.city = "kashmir" //np
+// muzaffar.name
+
+
+//different syntax
+class User2 {
+  readonly city: string = "kashmir"
+  constructor(
+    public email: string, 
+    public name: string,
+    private userId: number
+  ) {
+  }
+}
+const muzaffar2 = new User2("m@m.com", "muzaffar", 5465123)
